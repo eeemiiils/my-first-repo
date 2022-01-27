34356c87d6e539f0b7aecdbc8af747b48f5c115f
eeemiiils <98523780+eeemiiils@users.noreply.github.com>
2022-01-27 13:04:49 +0200

    Update bank.py
    Removed line

--- bank.py
+++ bank.py
@@ -7,7 +7,7 @@ class Transaction:
     def __init__(self, amount: float = 0, note: str = ''):
         self.amount = amount
         self.note = note
-        self.timestamp = datetime.datetime.now()
+        
 
 
 class Account:
